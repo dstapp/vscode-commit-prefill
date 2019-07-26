c41c2ce6566a34461eddc404cba0767d965d008e
David Prandzioch <david.prandzioch@saitow.ag>
2019-07-26 07:39:44 +0200

    Init

---
+++ src/extension.ts
@@ -0,0 +1,54 @@
+import * as vscode from 'vscode';
+
+let commitStatusBarItem: vscode.StatusBarItem;
+let commitPrefillText: string;
+let intervalId: any;
+
+export function activate(context: vscode.ExtensionContext) {
+  const gitExt = vscode.extensions.getExtension('vscode.git');
+
+  if (!gitExt) {
+    throw new Error('Git extension not found');
+  }
+
+  const editCommitTemplateCommandId = 'commitPrefill.editText';
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(editCommitTemplateCommandId, async () => {
+      const git = gitExt.exports.getAPI(1);
+      const selectedRepository = git.repositories.find((repo: any) => repo.ui.selected);
+
+      if (selectedRepository === null) {
+        return;
+      }
+
+      commitPrefillText =
+        (await vscode.window.showInputBox({
+          value: 'TICKET-',
+          placeHolder: 'TICKET-123'
+        })) || '';
+
+
+      commitStatusBarItem.text = commitPrefillText;
+
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+
+      intervalId = setInterval(() => {
+        if (selectedRepository.inputBox.value === '') {
+          selectedRepository.inputBox.value = `${commitPrefillText} `;
+        }
+      }, 10000);
+    })
+  );
+
+  commitStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  commitStatusBarItem.command = editCommitTemplateCommandId;
+  commitStatusBarItem.text = 'TICKET-????';
+  commitStatusBarItem.show();
+
+  context.subscriptions.push(commitStatusBarItem);
+}
+
+export function deactivate() {}
